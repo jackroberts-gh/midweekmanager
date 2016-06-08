db61b0e5e525afc5f07a0c0ba6623fdc05fca431
jzr1991 <pne_roberts@hotmail.com>
2016-06-08 23:44:32 +0100

    More UI
    etc

--- ng/posts/home.ctrl.js
+++ ng/posts/home.ctrl.js
@@ -1,9 +1,3 @@
 angular.module('app')
 .controller('HomeCtrl', function ($scope) {
-  $scope.photos = [
-    {id: 1, path: 'public/1.jpg'},
-    {id: 2, path: 'public/2.jpg'},
-    {id: 3, path: 'public/3.jpg'},
-    {id: 4, path: 'public/4.jpg'}
-  ]
 })
