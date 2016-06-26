41e8837f4757fbdf49957b01deff8656ef3edae0
jzr1991 <pne_roberts@hotmail.com>
2016-06-26 20:48:22 +0100

    Changing IU to use cards

--- gulp/css.js
+++ gulp/css.js
@@ -1,13 +1,24 @@
 var gulp = require('gulp')
 var stylus = require('gulp-stylus')
+var less = require('gulp-less')
 
-// Watches and builds everything in ng
-gulp.task('watch:css_js', ['css_js'], function() {
-  gulp.watch('styles/*.styl', ['css_js'])
+// Watches and builds everything in styles
+gulp.task('watch:cssstyls_js', ['cssstyls_js'], function() {
+  gulp.watch('styles/*.styl', ['cssstyls_js'])
 })
 
-gulp.task('css_js', function() {
+gulp.task('watch:cssless_js', ['cssless_js'], function() {
+  gulp.watch('styles/*.less', ['cssless_js'])
+})
+
+gulp.task('cssstyls_js', function() {
   gulp.src('styles/*.styl')
   .pipe(stylus())
+  .pipe(gulp.dest('assets'))
+})
+
+gulp.task('cssless_js', function() {
+  gulp.src('styles/*.less')
+  .pipe(less())
   .pipe(gulp.dest('assets'))
 })
--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,6 @@ fs.readdirSync(__dirname + '/gulp').forEach(function (task) {
   require('./gulp/' + task)
 })
 
-gulp.task('build', ['app_js', 'css_js'])
-gulp.task('watch', ['watch:all_js', 'watch:css_js'])
+gulp.task('build', ['app_js', 'cssstyls_js', 'cssless_js'])
+gulp.task('watch', ['watch:all_js', 'watch:cssstyls_js', 'watch:cssless_js'])
 gulp.task('dev', ['watch', 'dev:server'])
