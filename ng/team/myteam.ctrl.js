72f44f19725c06c629c38906323655ce70755b57
jzr1991 <pne_roberts@hotmail.com>
2016-07-01 23:27:08 +0100

    Cracked the dynamic image loading
    Done.

--- ng/team/myteam.ctrl.js
+++ ng/team/myteam.ctrl.js
@@ -1,12 +1,34 @@
 angular.module('app')
-.controller('MyTeamCtrl', function ($scope, UserSvc, TeamService, $routeParams) {
-  $scope.Goalkeeper = 0;
-  $scope.Defender = 0;
-  $scope.Midfielder = 0;
-  $scope.Forward = 0;
+.controller('MyTeamCtrl', function ($scope, UserSvc, TeamService, $routeParams, $parse) {
+
+  $scope.icons = [];
 
   TeamService.fetchOne($routeParams.team_id).success(function(team) {
       $scope.team = team;
+      angular.forEach($scope.team.players, function(value, key) {
+        if ($scope[value.position] === undefined) {
+          $parse(value.position).assign($scope, 1);}
+          else {
+            $scope[value.position]++;
+          }
+          $scope.icons.push($scope[value.position])
+      });
     })
+
+    $scope.positionOrder = function (item) {
+        switch (item.position) {
+            case 'Goalkeeper':
+                return 1;
+
+            case 'Defender':
+                return 2;
+
+            case 'Midfielder':
+                return 3;
+
+            case 'Forward':
+                return 4;
+        }
+    };
 
 })
