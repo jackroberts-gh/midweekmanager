015e13ee4f5378678c1982e41cb7b6a8da4c1b8c
jzr1991 <pne_roberts@hotmail.com>
2016-09-08 19:22:19 +0100

    Unique usernames and register error
    You now have to sign up with a unique email and user, and failing to
    provide either prompts a register error.

--- ng/user/register.ctrl.js
+++ ng/user/register.ctrl.js
@@ -7,6 +7,9 @@ angular.module('app')
       $scope.$emit('login', response.data)
       window.location.href = '/#/';
       })
+      .catch(function(err) {
+        $("#registererror").removeClass('hidden');
+      })
     }
 
     $scope.checkUsername = function() {
