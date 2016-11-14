8f1239395c823217657cb58f07f71628eea04387
jzr1991 <pne_roberts@hotmail.com>
2016-11-14 21:32:04 +0000

    need to bundle all css files but homepage done

---
+++ vendor.js
@@ -0,0 +1,8 @@
+require('expose?$!expose?jQuery!jquery');
+require('angular');
+require('angular-route');
+require('angular-toggle-switch');
+require('arrive');
+require('material');
+require('ripples');
+require('homepage');
--- webpack.config.js
+++ webpack.config.js
@@ -1,20 +1,35 @@
 var webpack = require('webpack');
+var path = require('path');
 
 module.exports = {
   context: __dirname,
   entry: {
-    vendor: ['jquery', 'angular','angular-route','angular-toggle-switch','arrive']
+    vendor: __dirname + "/vendor.js",
   },
   output: {
     path: __dirname + '/dist',
     filename: 'app.bundle.js'
   },
   plugins: [
-    new webpack.ProvidePlugin({
-       $: "jquery",
-       jQuery: "jquery",
-       jquery: "jquery"
-     }),
     new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
-  ]
+  ],
+  resolve: {
+      alias: {
+          jquery: path.resolve(__dirname, 'node_modules', 'jquery', 'dist', 'jquery.min.js'),
+          material: path.resolve(__dirname, 'node_modules', 'bootstrap-material-design', 'dist', 'js', 'material.min.js'),
+          ripples: path.resolve(__dirname, 'node_modules', 'bootstrap-material-design', 'dist', 'js', 'ripples.min.js'),
+          homepage: path.resolve(__dirname, 'dist', 'homepage.css'),
+      },
+      extensions: ['', '.js', '.jsx', '.css'],
+      modulesDirectories: [
+          'node_modules'
+        ]  
+  },
+  module: {
+    loaders: [
+        {
+          test: /\.css$/, loader: "style-loader!css-loader"
+        }
+    ]
+  }
 }
