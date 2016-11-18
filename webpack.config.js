74a76981e24918363498bde9ef7d80a9c4c2d0d5
jzr1991 <pne_roberts@hotmail.com>
2016-11-18 21:22:26 +0000

    chippy

--- webpack.config.js
+++ webpack.config.js
@@ -11,14 +11,19 @@ module.exports = {
     filename: 'app.bundle.js'
   },
   plugins: [
-    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js")
+    new webpack.optimize.CommonsChunkPlugin(/* chunkName= */"vendor", /* filename= */"vendor.bundle.js"),
+    new webpack.optimize.UglifyJsPlugin({ compress: { warnings: false } })
   ],
   resolve: {
       alias: {
           jquery: path.resolve(__dirname, 'node_modules', 'jquery', 'dist', 'jquery.js'),
           material: path.resolve(__dirname, 'node_modules', 'bootstrap-material-design', 'dist', 'js', 'material.min.js'),
           ripples: path.resolve(__dirname, 'node_modules', 'bootstrap-material-design', 'dist', 'js', 'ripples.min.js'),
-          homepage: path.resolve(__dirname, 'dist', 'homepage.css'),
+          bootstrap_css: path.resolve(__dirname, 'node_modules', 'bootstrap','dist','css','bootstrap.min.css'),
+          materialfullpalette_css: path.resolve(__dirname, 'node_modules', 'bootstrap-material-design','dist','css','material-fullpalette.css'),
+          material_css: path.resolve(__dirname, 'node_modules', 'bootstrap-material-design','dist','css','material.min.css'),
+          ripples_css: path.resolve(__dirname, 'node_modules', 'bootstrap-material-design','dist','css','ripples.min.css'),
+          roboto_css: path.resolve(__dirname, 'node_modules', 'bootstrap-material-design','dist','css','roboto.min.css'),
       },
       extensions: ['', '.js', '.jsx', '.css'],
       modulesDirectories: [
@@ -27,9 +32,16 @@ module.exports = {
   },
   module: {
     loaders: [
-        {
-          test: /\.css$/, loader: "style-loader!css-loader"
-        }
+        { test: /\.css$/, loader: "style-loader!css-loader" },
+        { test: /\.less$/, loader: "style-loader!css-loader!less-loader" },
+        { test: /\.styl$/, loader: "style-loader!css-loader!stylus-loader" },
+        { test: /.jsx?$/, loader: 'babel-loader', exclude: /node_modules/, query: { presets: ['es2015'] } },
+        { test: /\.png$/, loader: "url-loader?limit=100000" },
+        { test: /\.jpg$/, loader: "file-loader" },
+        { test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/font-woff'},
+        { test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=application/octet-stream' },
+        { test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, loader: 'file' },
+        { test: /\.svg(\?v=\d+\.\d+\.\d+)?$/, loader: 'url?limit=10000&mimetype=image/svg+xml' }
     ]
   }
 }
