a5aba271b4a80811e7730b51ae6eadb8f116f4b1
jzr1991 <pne_roberts@hotmail.com>
2016-12-03 21:20:49 +0000

    undo breaking change

--- gulp/vendor.js
+++ gulp/vendor.js
@@ -3,7 +3,7 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 gulp.task('vendor_js', function () {
-    return gulp.src(['node_modules/jquery/dist/jquery.js',
+    ggulp.src(['node_modules/jquery/dist/jquery.js',
         'node_modules/bootstrap/dist/js/bootstrap.js',
         'node_modules/angular/angular.js',
         'node_modules/angular-toggle-switch/angular-toggle-switch.js',
@@ -12,6 +12,5 @@ gulp.task('vendor_js', function () {
         'node_modules/bootstrap-material-design/dist/js/material.js',
         'node_modules/bootstrap-material-design/dist/js/ripples.js'])
         .pipe(concat('vendor.bundle.js'))
-        .pipe(uglify())
         .pipe(gulp.dest('dist'))
 })
