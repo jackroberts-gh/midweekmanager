8333a36329948eef1db89c3f5f76dc4af9698280
jzr1991 <pne_roberts@hotmail.com>
2016-12-03 20:36:55 +0000

    Arrive error perhaps

--- gulp/scripts.js
+++ gulp/scripts.js
@@ -4,28 +4,28 @@ var uglify = require('gulp-uglify');
 var ngAnnotate = require('gulp-ng-annotate');
 
 // Watches and builds everything in ng
-gulp.task('watch:all_js', ['app_js', 'vendor_js'], function() {
+gulp.task('watch:all_js', ['app_js', 'vendor_js'], function () {
   gulp.watch('ng/**/*.js', ['app_js'])
 })
 
-gulp.task('app_js', function() {
+gulp.task('app_js', function () {
   gulp.src(['ng/application/module.js', 'ng/**/*.js'])
-  .pipe(concat('app.bundle.js'))
-  .pipe(ngAnnotate())
-  .pipe(uglify({mangle: { toplevel: true } }))
-  .pipe(gulp.dest('dist'))
+    .pipe(concat('app.bundle.js'))
+    .pipe(ngAnnotate())
+    .pipe(uglify({ mangle: { toplevel: true } }))
+    .pipe(gulp.dest('dist'))
 })
 
-gulp.task('vendor_js', function() {
+gulp.task('vendor_js', function () {
   gulp.src(['node_modules/jquery/dist/jquery.js',
-            'node_modules/bootstrap/dist/js/bootstrap.js',
-            'node_modules/angular/angular.js',
-            'node_modules/angular-toggle-switch/angular-toggle-switch.js',
-            'node_modules/angular-route/angular-route.js',
-            'node_modules/arrive/minified/arrive.js',
-            'node_modules/bootstrap-material-design/dist/js/material.js',
-            'node_modules/bootstrap-material-design/dist/js/ripples.js'])
-        .pipe(concat('vendor.bundle.js'))
-        .pipe(uglify())
-        .pipe(gulp.dest('dist'))
+    'node_modules/bootstrap/dist/js/bootstrap.js',
+    'node_modules/angular/angular.js',
+    'node_modules/angular-toggle-switch/angular-toggle-switch.js',
+    'node_modules/angular-route/angular-route.js',
+    'node_modules/arrive/src/arrive.js',
+    'node_modules/bootstrap-material-design/dist/js/material.js',
+    'node_modules/bootstrap-material-design/dist/js/ripples.js'])
+    .pipe(concat('vendor.bundle.js'))
+    .pipe(uglify())
+    .pipe(gulp.dest('dist'))
 })
--- gulpfile.js
+++ gulpfile.js
@@ -7,7 +7,7 @@ fs.readdirSync(__dirname + '/gulp').forEach(function(task) {
 })
 
 gulp.task('default', ['dev'])
-gulp.task('build', ['app_js', 'app_css'])
+gulp.task('build', ['app_js', 'app_css', 'vendor_js'])
 gulp.task('watch', ['watch:all_js', 'watch:styles_js'])
 gulp.task('dev', ['watch'], function() {
     setTimeout(function() {
