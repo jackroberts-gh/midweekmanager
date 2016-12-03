a9a7fb6a1cd894b34083d2224bdb6a1d26f94ce9
jzr1991 <pne_roberts@hotmail.com>
2016-12-03 20:32:54 +0000

    Testing without vendor

--- gulpfile.js
+++ gulpfile.js
@@ -2,18 +2,18 @@ var gulp = require('gulp')
 var fs = require('fs')
 var nodemon = require('gulp-nodemon')
 
-fs.readdirSync(__dirname + '/gulp').forEach(function (task) {
-  require('./gulp/' + task)
+fs.readdirSync(__dirname + '/gulp').forEach(function(task) {
+    require('./gulp/' + task)
 })
 
 gulp.task('default', ['dev'])
-gulp.task('build', ['app_js', 'app_css', 'vendor_js'])
+gulp.task('build', ['app_js', 'app_css'])
 gulp.task('watch', ['watch:all_js', 'watch:styles_js'])
-gulp.task('dev', ['watch'], function () {
-  setTimeout(function () {
-    nodemon({
-      script: 'app.js',
-      ext: 'js styl less',
-    })
-  }, 500);
+gulp.task('dev', ['watch'], function() {
+    setTimeout(function() {
+        nodemon({
+            script: 'app.js',
+            ext: 'js styl less',
+        })
+    }, 500);
 })
