3561000bd1590b403fb8e5969b7cbc92438a9557
jzr1991 <pne_roberts@hotmail.com>
2016-12-07 20:57:09 +0000

    Fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -8,7 +8,7 @@ fs.readdirSync(__dirname + '/gulp').forEach(function (task) {
 
 gulp.task('default', ['dev'])
 gulp.task('build', ['app_js', 'app_css', 'vendor_js'])
-gulp.task('watch', ['watch:ng_js', 'watch:styles_js'])
+gulp.task('watch', ['watch:ng_js', 'watch:styles_js', 'vendor_js'])
 gulp.task('dev', ['watch'], function () {
     setTimeout(function () {
         nodemon({
