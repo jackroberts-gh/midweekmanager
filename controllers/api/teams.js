812d3b7cb014f0958b05419fe7f6988997f552a4
jzr1991 <pne_roberts@hotmail.com>
2016-12-11 17:42:11 +0000

    changes

--- controllers/api/teams.js
+++ controllers/api/teams.js
@@ -6,63 +6,72 @@ let router = require('express').Router();
 
 // \\ ** TEAM API ENDPOINT ** \\ //
 
-router.get('/', function (req, res, next) {
+router.get('/', function(req, res, next) {
   if (!req.headers['x-auth']) {
     return res.sendStatus(401)
   }
   if (req.auth.username) {
     Team.find()
       .sort('-date')
-      .exec(function (err, teams) {
-        if (err) { return next(err) }
+      .exec(function(err, teams) {
+        if (err) {
+          return next(err)
+        }
         res.json(teams)
       })
-  }
-  else {
+  } else {
     res.status(401)
   }
 })
 
 // Fetch teams I manage
-router.get('/:manager_id', function (req, res, next) {
+router.get('/:manager_id', function(req, res, next) {
   if (!req.headers['x-auth']) {
     return res.sendStatus(401)
   }
   if (req.auth.username) {
-    Team.find({ 'manager': req.params.manager_id })
+    Team.find({
+        'manager': req.params.manager_id
+      })
       .sort('-date')
-      .exec(function (err, teams) {
-        if (err) { return next(err) }
+      .exec(function(err, teams) {
+        if (err) {
+          return next(err)
+        }
         res.json(teams)
       })
-  }
-  else {
+  } else {
     res.status(401)
   }
 })
 
 // Get all teams my player ID is in
-router.get('/myteams/:players', function (req, res, next) {
+router.get('/myteams/:players', function(req, res, next) {
   if (!req.headers['x-auth']) {
     return res.sendStatus(401)
   }
   if (req.auth.username) {
     let ids = req.params.players.split(",");
 
-    Team.find({ 'players': { $in: ids } })
+    Team.find({
+        'players': {
+          $in: ids
+        }
+      })
       .sort('-date')
-      .exec(function (err, teams) {
-        if (err) { return next(err) }
+      .exec(function(err, teams) {
+        if (err) {
+          return next(err)
+        }
         res.json(teams)
       })
-  }
-  else {
+  } else {
     res.status(401)
   }
 })
 
 // Get team and populate information
-router.get('/team/:team_id', function (req, res, next) {
+router.get('/team/:team_id', function(req, res, next) {
   if (!req.headers['x-auth']) {
     return res.sendStatus(401)
   }
@@ -72,18 +81,19 @@ router.get('/team/:team_id', function (req, res, next) {
       .populate('manager')
       .populate('fixtures')
       .sort('-date')
-      .exec(function (err, teams) {
-        if (err) { return next(err) }
+      .exec(function(err, teams) {
+        if (err) {
+          return next(err)
+        }
         res.json(teams)
       })
-  }
-  else {
+  } else {
     res.status(401)
   }
 })
 
 // Create team
-router.post('/', function (req, res, next) {
+router.post('/', function(req, res, next) {
   if (!req.headers['x-auth']) {
     return res.sendStatus(401)
   }
@@ -94,94 +104,110 @@ router.post('/', function (req, res, next) {
       playday: req.body.playday,
       manager: req.body.manager
     })
-    team.save(function (err, team) {
-      if (err) { return next(err) }
+    team.save(function(err, team) {
+      if (err) {
+        return next(err)
+      }
       res.status(201).json(team)
-    })
-  }
-  else {
-    res.status(401)
-  }
-})
-
-// Assign player to team
-router.put('/player', function (req, res) {
-  if (!req.headers['x-auth']) {
-    return res.sendStatus(401)
-  }
-  if (req.auth.username) {
-    Team.findByIdAndUpdate(
-      req.body.team_id,
-      { $push: { players: { _id: req.body.player_id } } },
-      { new: true },
-      function (err, doc) {
-        if (err) {
-          res.send(err)
-        } else {
-          res.json(doc)
-        }
-      })
-  }
-  else {
-    res.status(401)
-  }
-})
-
-// Assign fixture to team_id
-router.put('/fixture', function (req, res) {
-  if (!req.headers['x-auth']) {
-    return res.sendStatus(401)
-  }
-  if (req.auth.username) {
-    Team.findByIdAndUpdate(
-      req.body.team_id,
-      { $push: { fixtures: { _id: req.body.fixture_id } } },
-      { new: true },
-      function (err, doc) {
-        if (err) {
-          res.send(err)
-        } else {
-          res.json(doc)
-        }
-      })
-  }
-  else {
-    res.status(401)
-  }
-})
-
-router.get('/:team_id', (function (req, res) {
-  if (!req.headers['x-auth']) {
-    return res.sendStatus(401)
-  }
-  if (req.auth.username) {
-    Team.findById(req.params.team_id, function (err, team) {
-      if (err) { res.send(err) }
-      res.json(team)
     })
   } else {
     res.status(401)
   }
 })
-)
 
-// Currently unused below
-
-router.put('/:team_id', function (req, res) {
+// Assign player to team
+router.put('/player', function(req, res) {
   if (!req.headers['x-auth']) {
     return res.sendStatus(401)
   }
   if (req.auth.username) {
-    Team.findById(req.params.team_id, function (err, team) {
+    Team.findByIdAndUpdate(
+      req.body.team_id, {
+        $push: {
+          players: {
+            _id: req.body.player_id
+          }
+        }
+      }, {
+        new: true
+      },
+      function(err, doc) {
+        if (err) {
+          res.send(err)
+        } else {
+          res.json(doc)
+        }
+      })
+  } else {
+    res.status(401)
+  }
+})
+
+// Assign fixture to team_id
+router.put('/fixture', function(req, res) {
+  if (!req.headers['x-auth']) {
+    return res.sendStatus(401)
+  }
+  if (req.auth.username) {
+    Team.findByIdAndUpdate(
+      req.body.team_id, {
+        $push: {
+          fixtures: {
+            _id: req.body.fixture_id
+          }
+        }
+      }, {
+        new: true
+      },
+      function(err, doc) {
+        if (err) {
+          res.send(err)
+        } else {
+          res.json(doc)
+        }
+      })
+  } else {
+    res.status(401)
+  }
+})
+
+router.get('/:team_id', (function(req, res) {
+  if (!req.headers['x-auth']) {
+    return res.sendStatus(401)
+  }
+  if (req.auth.username) {
+    Team.findById(req.params.team_id, function(err, team) {
+      if (err) {
+        res.send(err)
+      }
+      res.json(team)
+    })
+  } else {
+    res.status(401)
+  }
+}))
+
+// Currently unused below
+
+router.put('/:team_id', function(req, res) {
+  if (!req.headers['x-auth']) {
+    return res.sendStatus(401)
+  }
+  if (req.auth.username) {
+    Team.findById(req.params.team_id, function(err, team) {
       if (err)
         res.send(err);
 
       team.username = req.body.username;
       team.body = req.body.body;
 
-      team.save(function (err) {
-        if (err) { res.send(err) }
-        res.json({ message: 'Team updated!' });
+      team.save(function(err) {
+        if (err) {
+          res.send(err)
+        }
+        res.json({
+          message: 'Team updated!'
+        });
       })
     })
   } else {
@@ -189,13 +215,16 @@ router.put('/:team_id', function (req, res) {
   }
 })
 
-router.delete('/:team_id', function (req, res) {
+router.delete('/:team_id', function(req, res) {
   Team.remove({
     _id: req.params.team_id
-  }, function (err, team) {
-    if (err)
+  }, function(err, team) {
+    if (err) {
       res.send(err);
-    res.json({ message: 'Successfully deleted' });
+    }
+    res.json({
+      message: 'Successfully deleted'
+    });
   });
 })
 
--- gulp/css.js
+++ gulp/css.js
@@ -6,31 +6,32 @@ const less = require('gulp-less')
 const concat = require('gulp-concat');
 
 // Watches and builds everything in styles
-gulp.task('watch:styles_js', ['app_css'], function () {
-    gulp.watch(['styles/*.less', 'styles/*.styl'], ['css_concat'])
+gulp.task('watch:styles_js', ['app_css'], function() {
+  gulp.watch(['styles/*.less', 'styles/*.styl'], ['app_css'])
 })
 
-gulp.task('app_css', ['cssless_js', 'cssstyls_js'], function () {
-    gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css',
-        'node_modules/bootstrap-material-design/dist/css/material.css',
-        'node_modules/bootstrap-material-design/dist/css/ripples.css',
-        'node_modules/bootstrap-material-design/dist/css/roboto.css',
-        'node_modules/angular-toggle-switch/angular-toggle-switch.css',
-        'node_modules/font-awesome/css/font-awesome.css',
-        'styles/css/homepage.css',
-        'styles/css/card.css'])
-        .pipe(concat('styles.bundle.css'))
-        .pipe(gulp.dest('dist'))
+gulp.task('app_css', ['cssless_js', 'cssstyls_js'], function() {
+  gulp.src(['node_modules/bootstrap/dist/css/bootstrap.css',
+      'node_modules/bootstrap-material-design/dist/css/material.css',
+      'node_modules/bootstrap-material-design/dist/css/ripples.css',
+      'node_modules/bootstrap-material-design/dist/css/roboto.css',
+      'node_modules/angular-toggle-switch/angular-toggle-switch.css',
+      'node_modules/font-awesome/css/font-awesome.css',
+      'styles/css/homepage.css',
+      'styles/css/card.css'
+    ])
+    .pipe(concat('styles.bundle.css'))
+    .pipe(gulp.dest('dist'))
 })
 
-gulp.task('cssstyls_js', function () {
-    gulp.src('styles/*.styl')
-        .pipe(stylus())
-        .pipe(gulp.dest('styles/css'))
+gulp.task('cssstyls_js', function() {
+  gulp.src('styles/*.styl')
+    .pipe(stylus())
+    .pipe(gulp.dest('styles/css'))
 })
 
-gulp.task('cssless_js', function () {
-    gulp.src('styles/*.less')
-        .pipe(less())
-        .pipe(gulp.dest('styles/css'))
+gulp.task('cssless_js', function() {
+  gulp.src('styles/*.less')
+    .pipe(less())
+    .pipe(gulp.dest('styles/css'))
 })
